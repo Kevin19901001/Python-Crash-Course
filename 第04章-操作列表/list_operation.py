db09e0d54647c9bd885b6bf9de83547761a81c54
e <huanqing19901001@163.com>
2021-07-05 21:19:43 +0800

    Some new content

--- 第04章-操作列表/list_operation.py
+++ 第04章-操作列表/list_operation.py
@@ -90,3 +90,11 @@ print(sum(list))
 list = range(1, 20, 2)
 for value in list:
 	print(value)
+
+
+# 3的倍数：
+# 创建一个列表，其中包含3～30能被3整除的数，
+# 再使用一个for循环将这个列表中的数打印出来
+list = range(3, 31, 3)
+for value in list:
+	print(value)
