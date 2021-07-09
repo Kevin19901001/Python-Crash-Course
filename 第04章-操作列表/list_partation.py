14aab07d9a26e5794b158e7516210e7a5677e313
e <huanqing19901001@163.com>
2021-07-09 22:39:28 +0800

    Some new content

--- 第04章-操作列表/list_partation.py
+++ 第04章-操作列表/list_partation.py
@@ -27,3 +27,13 @@ print('My favorite foods are:')
 print(my_foods)
 print("\nMy friend's favorite foods are:")
 print(friend_foods)
+
+
+# 练习：
+# 切片 - 选择你在本章编写的一个程序，在末尾
+# 添加几行代码，以完成如下任务：
+# 打印消息“The first thress items in the list are:”，
+# 再使用切片来打印列表的前3个元素
+print("The first three items in the list are:")
+for value in players[:3]:
+	print(value)
