4ad2ce02a5140f1b2fee55a89736ae3666c3318d
e <huanqing19901001@163.com>
2021-07-12 21:31:11 +0800

    Some new content

---
+++ 第04章-操作列表/foods.py
@@ -0,0 +1,11 @@
+# 使用多个循环
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+for my_food in my_foods:
+	print(my_food)
+
+print('')
+
+for friend_food in friend_foods:
+	print(friend_food)
