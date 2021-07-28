6512f76765d39e366a1601764a5b95a96e9c5463
Kevin <huanqing19901001@163.com>
2021-07-28 22:21:46 +0800

    Add some new content.

--- 第05章-if语句/condition_demo.py
+++ 第05章-if语句/condition_demo.py
@@ -140,3 +140,15 @@ elif age < 65:
 else:
 	price = 20
 print(f"Your admission cost is ${price}.")
+
+
+# 5.3.6 测试多个条件
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+	print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+	print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+	print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
