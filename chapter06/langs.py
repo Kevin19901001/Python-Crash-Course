b62ba8c38b618410ce7d29f7f8f1ebc314e6d8ec
Kevin <huanqing19901001@163.com>
2022-08-02 20:05:49 +0800

    Update langs.py
    a little changes

--- chapter06/langs.py
+++ chapter06/langs.py
@@ -1,6 +1,5 @@
 # favorite languages
-favorite_languages = {“jen”:”python”, “sarah”:”c”, “edward”:”ruby”, “phil”:”puthon”}
-
+favorite_languages = {'Jen':'python', 'Sarah':'c', 'Edward':'ruby', 'phil':'puthon'}
 # 遍历
 for name in favorite_languages.keys():
     print(name.title())
