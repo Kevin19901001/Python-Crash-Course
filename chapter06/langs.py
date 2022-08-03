7601182ccdee3016829f545ef1846c2ac280f02b
fanhuanqing <huanqing19901001@163.com>
2022-08-03 19:19:14 +0800

    a little new content about dictionary

--- chapter06/langs.py
+++ chapter06/langs.py
@@ -1,5 +1,9 @@
 # favorite languages
 favorite_languages = {'Jen':'python', 'Sarah':'c', 'Edward':'ruby', 'phil':'puthon'}
-# 遍历
+# 遍历键
 for name in favorite_languages.keys():
     print(name.title())
+
+# 遍历值
+for value in favorite_languages.values():
+    print(value.title())
