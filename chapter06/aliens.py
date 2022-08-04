a03d138582668bf24e2909c06b72890acdafc023
fanhuanqing <huanqing19901001@163.com>
2022-08-04 19:36:35 +0800

    a little new content

---
+++ chapter06/aliens.py
@@ -0,0 +1,9 @@
+# 嵌套
+# 1.列表嵌套字典
+alien_0 = {'color':'green', 'points':15}
+alien_1 = {'color':'yellow', 'points':20}
+alien_2 = {'color':'red', 'points':30}
+
+aliens = [alien_0, alien_1, alien_2]
+
+print(aliens)
