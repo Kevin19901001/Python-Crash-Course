cb84b1598089ceeac27200aaa0432f2a52cfb5fd
fanhuanqing <huanqing19901001@163.com>
2022-08-14 20:49:59 +0800

    a little new content

---
+++ chapter07/cities.py
@@ -0,0 +1,11 @@
+# 使用break推出循环
+prompt = "\nPlease enter the name of a city you have visited."
+prompt = "\n(Enter 'quit' when you are finished.)"
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")
