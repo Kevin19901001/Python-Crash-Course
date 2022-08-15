08272189170492e30edd3227e03b5b7b54e00875
fanhuanqing <huanqing19901001@163.com>
2022-08-15 19:27:07 +0800

    a little new content

--- chapter07/counting.py
+++ chapter07/counting.py
@@ -3,3 +3,16 @@ current_count = 1
 while current_count <= 5:
     print(current_count)
     current_count += 1
+
+
+# 在循环中使用continue
+print("\n在循环中使用continue:")
+current_num = 0
+
+while current_num < 10:
+    current_num += 1
+
+    if current_num % 2 == 0:
+        continue
+
+    print(current_num)
