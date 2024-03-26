7d3c99c8c93dc1dce84447cca7ddb46178e4741d
huanqing19901001@163.com <huanqing19901001@163.com>
2024-03-26 22:01:36 +0800

    Update case05.

--- chapter08/case05_trans_any_params.py
+++ chapter08/case05_trans_any_params.py
@@ -1,9 +1,10 @@
-
 """传递任意数量的实参"""
+
 
 def make_pizza(*toppings):
     """打印顾客的所有配料"""
     print(toppings)
+
 
 make_pizza('pepperoni')
 make_pizza('mushrooms', 'green peppers', 'extra cheese')
@@ -14,26 +15,30 @@ def make_pizza_again(*toppings):
     for topping in toppings:
         print(f'- {topping}')
 
+
 make_pizza_again('pepperoni')
 make_pizza_again('mushrooms', 'green peppers', 'extra cheese')
 
+"""结合使用位置实参和任意数量实参"""
 
-"""结合使用位置实参和人一数量实参"""
+
 def make_pizza_again_and_again(size, *toppings):
     print(f'\nMaking a {size}-inch pizza with following toppings:')
     for topping in toppings:
         print(f'- {topping}')
 
+
 make_pizza_again_and_again(16, 'pepperoni')
 make_pizza_again_and_again(12, 'mushrooms', 'green peppers', 'extra cheese')
 
-
 """使用任意数量的关键字实参"""
+
+
 def build_profile(first, last, **user_info):
     user_info['first_name'] = first
     user_info['last_name'] = last
     return user_info
 
+
 user_profile = build_profile('albert', 'einstein', location='princeton', field='physics')
 print(user_profile)
-
